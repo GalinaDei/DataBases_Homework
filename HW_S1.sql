4b55aa7eaf3f9bc62c42f1af5cb289b88ffda119
Galina <gal-dej@yandex.ru>
2023-04-17 11:30:16 +0300

    Seminar1_homework

---
+++ HW_S1.sql
@@ -0,0 +1,14 @@
+-- comments --
+/* 
+Стили:
+PascalCase: PrintArray
+camelCase: firstValue
+kebab-case: my-first-var
+
+Для SQL лучше всего подойдет:
+_snake_case, snake_case: first_value
+UPPER_CASE_SNAKE_CASE: FIRST_VALUE
+*/ 
+CREATE DATABASE IF NOT EXISTS mobile_phones;
+USE mobile_phones;
+
